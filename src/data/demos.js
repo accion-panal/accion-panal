964f5ffe8124329ab55e34d9087f4765270b1ff3
fabian <fabian.salas.astete@gmail.com>
2023-09-20 14:53:35 -0300

    issues:change names demos b-1 to b-6 and a-1 to a-3

--- src/data/demos.js
+++ src/data/demos.js
@@ -1,37 +1,37 @@
 export const demosBasicData = [
   {
     id: 1,
-    name: 'DEMO PAGINA BASICA 1',
+    name: 'DEMO PÁGINA B-1',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658407/accion-panal/demos/wb-1_hha5o0.png',
     href: 'https://pagina-web-b1.partnersadvisers.info/',
   },
   {
     id: 2,
-    name: 'DEMO PAGINA BASICA 2',
+    name: 'DEMO PÁGINA B-2',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658408/accion-panal/demos/wb-2_gqxbdl.png',
     href: 'https://pagina-web-b2.partnersadvisers.info/',
   },
   {
     id: 3,
-    name: 'DEMO PAGINA BASICA 3',
+    name: 'DEMO PÁGINA B-3',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658407/accion-panal/demos/wb-3_om0eqp.png',
     href: 'https://pagina-web-b3.partnersadvisers.info/',
   },
   {
     id: 4,
-    name: 'DEMO PAGINA BASICA 4',
+    name: 'DEMO PÁGINA B-4',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658407/accion-panal/demos/wb-4_kozswv.png',
     href: 'https://pagina-web-b4.partnersadvisers.info/',
   },
   {
     id: 5,
-    name: 'DEMO PAGINA BASICA 5',
+    name: 'DEMO PÁGINA B-5',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658407/accion-panal/demos/wb-5_t0cmez.png',
     href: 'https://pagina-web-b5.partnersadvisers.info/',
   },
   {
     id: 6,
-    name: 'DEMO PAGINA BASICA 6',
+    name: 'DEMO PÁGINA B-6',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658528/accion-panal/demos/wp-6_o0gehe.png',
     href: 'https://pagina-web-b6.partnersadvisers.info/',
   },
@@ -40,19 +40,19 @@ export const demosBasicData = [
 export const demosAdvancedData = [
   {
     id: 1,
-    name: 'DEMO PAGINA AVANZADA 1',
+    name: 'DEMO PÁGINA A-1',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683665770/accion-panal/demos/wa-1_omhece.png',
     href: 'https://pagina-web-a1.partnersadvisers.info/',
   },
   {
     id: 2,
-    name: 'DEMO PAGINA AVANZADA 2',
+    name: 'DEMO PÁGINA A-2',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683665833/accion-panal/demos/wa-2_qezs07.png',
     href: 'https://pagina-web-a2.partnersadvisers.info/',
   },
   {
     id: 3,
-    name: 'DEMO PAGINA AVANZADA 3',
+    name: 'DEMO PÁGINA A-3',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683665874/accion-panal/demos/wa-3_adpjkr.png',
     href: 'https://pagina-web-a3.partnersadvisers.info/',
   },
